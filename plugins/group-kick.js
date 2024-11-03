ea931c04d5025b7b7a03772a9b8f2ea839f60478
DexterZ9 <dexterdp909@gmail.com>
2024-11-02 19:14:39 -0600

    Update group-kick.js

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -1,12 +1,20 @@
 let handler = async (m, { conn, participants, usedPrefix, command, isROwner }) => {
-	
-let kickte = `_Menciona al usuario que deseas eliminar._`
+    let kickte = `_Menciona al usuario que deseas eliminar._`
 
-if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)}) 
-let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
-let owr = m.chat.split`-`[0]
-await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`🍭 Usuario eliminado.`)
+    // Verifica si se mencionó un usuario o si se respondió a un mensaje.
+    if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)}) 
+
+    let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
+    let botNumber = conn.user.jid.split('@')[0]  // Obtiene el número del bot
+
+    // Verifica si el usuario a expulsar es el bot
+    if (user.includes(botNumber)) {
+        return m.reply(`⚠️ No puedo expulsarme a mí mismo.`);
+    }
+
+    // Realiza la acción de expulsión
+    await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
+    m.reply(`🍭 Usuario eliminado.`)
 }
 
 handler.help = ['kick @user']
@@ -17,3 +25,4 @@ handler.group = true
 handler.botAdmin = true
 
 export default handler
+	
