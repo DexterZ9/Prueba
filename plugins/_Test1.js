85b5f31a11d18ef12c7d8cce4cb255c479391373
DexterZ9 <dexterdp909@gmail.com>
2024-11-05 09:47:55 -0600

    Update _Test1.js

--- plugins/_Test1.js
+++ plugins/_Test1.js
@@ -54,7 +54,8 @@ ${readMore}
 ├ ⓘ _Descargar archivos de Mediafire_
 ╰─❮ 🪻 ❯
     `.trim()
-        conn.reply(m.chat, menu, m, {contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: '✿ 𝐀𝐢𝐫𝐢 ✿', body: '𝙴𝚕 𝚖𝚎𝚓𝚘𝚛 𝚋𝚘𝚝 𝚍𝚎 𝚆𝚑𝚊𝚝𝚜𝙰𝚙𝚙 ✅', previewType: 0, "renderLargerThumbnail": true, thumbnail: icons.getRandom(), sourceUrl: 'https://github.com/Rudyrex/Airi-Bot'}}});
+    conn.sendAiri(m.chat, 'title 🐼', 'body 🐢', menu, icon1, 'www.google.com', m );
+    //conn.reply(m.chat, menu, m, {contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: '✿ 𝐀𝐢𝐫𝐢 ✿', body: '𝙴𝚕 𝚖𝚎𝚓𝚘𝚛 𝚋𝚘𝚝 𝚍𝚎 𝚆𝚑𝚊𝚝𝚜𝙰𝚙𝚙 ✅', previewType: 0, "renderLargerThumbnail": true, thumbnail: icons.getRandom(), sourceUrl: 'https://github.com/Rudyrex/Airi-Bot'}}});
 }
 handler.command = ['menu', 'help']
 export default handler
