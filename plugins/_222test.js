f103f09bffb1e187b401cef779c5082bfc05468d
DexterZ9 <dexterdp909@gmail.com>
2024-11-09 12:52:38 -0600

    Update _222test.js

--- plugins/_222test.js
+++ plugins/_222test.js
@@ -8,14 +8,14 @@ let handler = async (m, { conn }) => {
     let thum = 'https://f.uguu.se/bZEQKzcr.jpg'
     let text = `Hola 🐼`.trim()
     //await conn.sendAiri(m.chat, 'Título Ejemplo', 'Cuerpo Ejemplo', 'Texto Ejemplo', 'https://f.uguu.se/bZEQKzcr.jpg', 'https://ejemplo.com', m);
-        
-    conn.reply(m.chat, text, m, {contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: '✿ 𝐀𝐢𝐫𝐢 ✿', body: '𝙴𝚕 𝚖𝚎𝚓𝚘𝚛 𝚋𝚘𝚝 𝚍𝚎 𝚆𝚑𝚊𝚝𝚜𝙰𝚙𝚙 ✅', previewType: 0, "renderLargerThumbnail": true, thumbnail: icon1, sourceUrl: 'https://github.com/Rudyrex/Airi-Bot'}}});
+        m.reply('🐼');
+    //conn.reply(m.chat, text, m, {contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: '✿ 𝐀𝐢𝐫𝐢 ✿', body: '𝙴𝚕 𝚖𝚎𝚓𝚘𝚛 𝚋𝚘𝚝 𝚍𝚎 𝚆𝚑𝚊𝚝𝚜𝙰𝚙𝚙 ✅', previewType: 0, "renderLargerThumbnail": true, thumbnail: icon1, sourceUrl: 'https://github.com/Rudyrex/Airi-Bot'}}});
     } catch (e) {
         m.reply(e.message)
     }
  }
  
-handler.command = ['y']
+handler.command = ['test8'];
 export default handler
 
 const more = String.fromCharCode(8206)
