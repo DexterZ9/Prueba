1e7371fa3c1fea1347662d7f8eeb7a4078bc390a
DexterZ9 <dexterdp909@gmail.com>
2024-11-10 20:19:06 -0600

    Update _222test.js

--- plugins/_222test.js
+++ plugins/_222test.js
@@ -4,12 +4,44 @@ import { iconRandom } from '../exports.js'
 let handler = async (m, { conn }) => {
     let icn = iconRandom;
     try {
+let text = `Hola 🐼`.trim()
+function generateRandomCode(length = 6) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let code = '';
+    for (let i = 0; i < length; i++) {
+        code += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return code;
+}
+
+const randomTitle = `✿ 𝐀𝐢𝐫𝐢 ✿ - ${generateRandomCode()}`;
+
+conn.reply(m.chat, text, m, {
+    contextInfo: {
+        externalAdReply: {
+            mediaUrl: null,
+            mediaType: 1,
+            description: null,
+            title: randomTitle,
+            body: '𝙴𝚕 𝚖𝚎𝚓𝚘𝚛 𝚋𝚘𝚝 𝚍𝚎 𝚆𝚑𝚊𝚝𝚜𝙰𝚙𝚙 ✅',
+            previewType: 0,
+            renderLargerThumbnail: true,
+            thumbnail: icn,
+            sourceUrl: 'https://github.com/Rudyrex/Airi-Bot'
+        }
+    }
+});
+        
+
+        
+        /*
     let name = await conn.getName(m.sender)
     let thum = 'https://f.uguu.se/bZEQKzcr.jpg'
     let text = `Hola 🐼`.trim()
     await conn.sendAiri(m.chat, 'Título Ejemplo', 'Cuerpo Ejemplo', text, icn, 'https://ejemplo.com', m);
         m.reply('🐼');
     //conn.reply(m.chat, text, m, {contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: '✿ 𝐀𝐢𝐫𝐢 ✿', body: '𝙴𝚕 𝚖𝚎𝚓𝚘𝚛 𝚋𝚘𝚝 𝚍𝚎 𝚆𝚑𝚊𝚝𝚜𝙰𝚙𝚙 ✅', previewType: 0, "renderLargerThumbnail": true, thumbnail: icn, sourceUrl: 'https://github.com/Rudyrex/Airi-Bot'}}});
+   */
     } catch (e) {
         m.reply(e.message)
     }
