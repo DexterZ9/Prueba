756f848a3f27ca5274ada3838a9ca67f03011759
DexterZ9 <dexterdp909@gmail.com>
2024-11-16 09:19:14 -0600

    Create downloader-ytmp4doc.js

---
+++ plugins/downloader-ytmp4doc.js
@@ -0,0 +1,27 @@
+
+import fetch from 'node-fetch';
+
+
+let handler = async (m, { text, conn, args, usedPrefix, command }) => {
+    
+    if (!args[0]) return await conn.reply(m.chat, `⚠️ Agrega un enlace de *YouTube*`, m);
+    let yturl = args[0]
+    m.react('⏳')
+    conn.reply(m.chat, '⏳ Descargando el video...', m);
+    
+    try {
+        let api1 = await fetch(`https://deliriussapi-oficial.vercel.app/download/ytmp4?url=${yturl}`)    
+        let result1 = await api1.json()
+        let title1 = result1.data.title;
+        let downloadUrl1 = result1.data.download.url;
+        
+        await m.react('✅')
+        await conn.sendMessage(m.chat, {document: {url: downloadUrl1}, caption: null, mimetype: 'video/mp4', fileName: `${title1}.mp4`}, {quoted: m});
+        //await conn.sendMessage(m.chat, { video: { url: downloadUrl1 }, fileName: `Facebook.mp4`, mimetype: 'video/mp4', caption: null }, { quoted: m });
+    } catch (e1) {
+        await conn.reply(m.chat, e2.message, m);
+    }
+};
+
+handler.command = ['ytvdoc', 'ytmp4doc',];
+export default handler;
