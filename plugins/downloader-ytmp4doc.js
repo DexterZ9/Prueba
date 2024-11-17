7ff3745e349970a09646a6408c6e8b94c15f0d81
DexterZ9 <dexterdp909@gmail.com>
2024-11-17 08:34:18 -0600

    Update downloader-ytmp4doc.js

--- plugins/downloader-ytmp4doc.js
+++ plugins/downloader-ytmp4doc.js
@@ -1,19 +1,28 @@
-import Scraper from "@SumiFX/Scraper"
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) return m.reply('⚠️ Ingresa el enlace del vídeo de YouTube junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://youtu.be/QSvaCSt8ixs`)
-    if (!args[0].match(/youtu/gi)) return conn.reply(m.chat, `Verifica que el enlace sea de YouTube.`, m)
 
-    try {
-    let { title, size, quality, thumbnail, dl_url } = await Scraper.ytmp4(args[0])
-    if (size.includes('GB') || size.replace(' MB', '') > 250) { return await m.reply('El archivo pesa mas de 250 MB, se canceló la Descarga.')}
+import fetch from 'node-fetch';
+
+
+let handler = async (m, { text, conn, args, usedPrefix, command }) => {
     
-    await conn.sendMessage(m.chat, {document: {url: dl_url}, caption: null, mimetype: 'video/mp4', fileName: `${title}.mp4`}, {quoted: m});
-    } catch {
+    if (!args[0]) return await conn.reply(m.chat, `⚠️ Agrega un enlace de *YouTube*`, m);
+    let yturl = args[0]
+    m.react('⏳')
+    conn.reply(m.chat, '⏳ Descargando el video...', m);
+    
+    try {
+        let api1 = await fetch(`https://deliriussapi-oficial.vercel.app/download/ytmp4?url=${yturl}`)    
+        let result1 = await api1.json()
+        let title1 = result1.data.title;
+        let downloadUrl1 = result1.data.download.url;
         
+        await m.react('✅')
+        await conn.sendMessage(m.chat, {document: {url: downloadUrl1}, caption: null, mimetype: 'video/mp4', fileName: `${title1}.mp4`}, {quoted: m});
+        
+    } catch (e1) {
+        await conn.reply(m.chat, e2.message, m);
     }
-}
+};
 
-handler.command = ['ytmp4doc', 'ytvdoc']
-
-export default handler
+handler.command = ['ytvdoc', 'ytmp4doc',];
+export default handler;
