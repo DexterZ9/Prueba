9d605ab7587712d62e50889369c33aa60beb2ea8
DexterZ9 <dexterdp909@gmail.com>
2024-11-17 19:59:36 -0600

    Update _Test1.js

--- plugins/_Test1.js
+++ plugins/_Test1.js
@@ -2,11 +2,11 @@
 let handler = async (m, { conn }) => {
     try {
     let name = await conn.getName(m.sender)
-    let thum = 'https://f.uguu.se/bZEQKzcr.jpg'
+    let thum = 'https://qu.ax/MPeCD.jpg'
     let text = `Hola 🐼`.trim()
     //await conn.sendAiri(m.chat, 'Título Ejemplo', 'Cuerpo Ejemplo', 'Texto Ejemplo', 'https://f.uguu.se/bZEQKzcr.jpg', 'https://ejemplo.com', m);
         
-    conn.reply(m.chat, menu, m, {contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: '✿ 𝐀𝐢𝐫𝐢 ✿', body: '𝙴𝚕 𝚖𝚎𝚓𝚘𝚛 𝚋𝚘𝚝 𝚍𝚎 𝚆𝚑𝚊𝚝𝚜𝙰𝚙𝚙 ✅', previewType: 0, "renderLargerThumbnail": true, thumbnail: icons.getRandom(), sourceUrl: 'https://github.com/Rudyrex/Airi-Bot'}}});
+    conn.reply(m.chat, menu, m, {contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: '✿ 𝐀𝐢𝐫𝐢 ✿', body: '𝙴𝚕 𝚖𝚎𝚓𝚘𝚛 𝚋𝚘𝚝 𝚍𝚎 𝚆𝚑𝚊𝚝𝚜𝙰𝚙𝚙 ✅', previewType: 0, "renderLargerThumbnail": true, thumbnail: thumb, sourceurl: thumb}}});
     } catch (e) {
         m.reply(e.message)
     }
