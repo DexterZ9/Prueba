1d43ae299c67a00d433194c71f1270bf1050c48f
DexterZ9 <dexterdp909@gmail.com>
2024-11-17 20:45:20 -0600

    Update _Test1.js

--- plugins/_Test1.js
+++ plugins/_Test1.js
@@ -5,8 +5,17 @@ let handler = async (m, { conn }) => {
     let thum = 'https://qu.ax/MPeCD.jpg'
     let text = `Hola 🐼`.trim()
     //await conn.sendAiri(m.chat, 'Título Ejemplo', 'Cuerpo Ejemplo', 'Texto Ejemplo', 'https://f.uguu.se/bZEQKzcr.jpg', 'https://ejemplo.com', m);
+
+        await conn.sendAiri(
+    m.chat,
+    'Título de Prueba',
+    'Descripción de Prueba',
+    'Texto Opcional',
+    'https://qu.ax/MPeCD.jpg', // URL de la imagen como thumbnail
+    'https://qu.ax/MPeCD.jpg', // Fuente del enlace
+    m);
         
-    conn.reply(m.chat, menu, m, {contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: '✿ 𝐀𝐢𝐫𝐢 ✿', body: '𝙴𝚕 𝚖𝚎𝚓𝚘𝚛 𝚋𝚘𝚝 𝚍𝚎 𝚆𝚑𝚊𝚝𝚜𝙰𝚙𝚙 ✅', previewType: 0, "renderLargerThumbnail": true, thumbnail: thumb, sourceurl: thumb}}});
+    //conn.reply(m.chat, menu, m, {contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: '✿ 𝐀𝐢𝐫𝐢 ✿', body: '𝙴𝚕 𝚖𝚎𝚓𝚘𝚛 𝚋𝚘𝚝 𝚍𝚎 𝚆𝚑𝚊𝚝𝚜𝙰𝚙𝚙 ✅', previewType: 0, "renderLargerThumbnail": true, thumbnail: thumb, sourceurl: thumb}}});
     } catch (e) {
         m.reply(e.message)
     }
