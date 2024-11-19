71f461720da0d3a74f1924d10fb67d16aa52c544
DexterZ9 <dexterdp909@gmail.com>
2024-11-19 08:27:52 -0600

    Update exports.js

--- exports.js
+++ exports.js
@@ -10,3 +10,16 @@ export const getRandomIcon = () => {
     const randomIcon = Math.floor(Math.random() * iconUrls.length);
     return iconUrls[randomIcon];
 };
+
+const thumbUrls = [
+    "https://raw.githubusercontent.com/DexterZ9/Prueba/refs/heads/main/src/img/thumbnail1.jpg",
+    "https://raw.githubusercontent.com/DexterZ9/Prueba/refs/heads/main/src/img/thumbnail2.jpg",
+    "https://raw.githubusercontent.com/DexterZ9/Prueba/refs/heads/main/src/img/thumbnail3.jpg",
+    "https://raw.githubusercontent.com/DexterZ9/Prueba/refs/heads/main/src/img/thumbnail4.jpg",
+    "https://raw.githubusercontent.com/DexterZ9/Prueba/refs/heads/main/src/img/thumbnail5.jpg"
+];
+
+export const getRandomThumb = () => {
+    const randomThumb = Math.floor(Math.random() * thumbUrls.length);
+    return thumbUrls[randomThumb];
+};
