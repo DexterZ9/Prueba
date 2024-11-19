a3bf3b9ccb35dab05c1db37dc767f7db73ff3783
DexterZ9 <dexterdp909@gmail.com>
2024-11-19 08:24:00 -0600

    Update exports.js

--- exports.js
+++ exports.js
@@ -1,9 +1,11 @@
 const iconUrls = [
-    "https://qu.ax/MPeCD.jpg",
+    "https://raw.githubusercontent.com/DexterZ9/Prueba/refs/heads/main/src/img/icon1.jpg",
+    "https://raw.githubusercontent.com/DexterZ9/Prueba/refs/heads/main/src/img/icon2.jpg",
+    "https://raw.githubusercontent.com/DexterZ9/Prueba/refs/heads/main/src/img/icon3.jpg",
     "https://raw.githubusercontent.com/DexterZ9/Prueba/refs/heads/main/media/img/icon4.jpg",
     "https://raw.githubusercontent.com/DexterZ9/Prueba/refs/heads/main/media/img/icon5.jpg"
 ];
-console.log('exports cargados');
+console.log('exports load √');
 export const getRandomIcon = () => {
     const randomIcon = Math.floor(Math.random() * iconUrls.length);
     return iconUrls[randomIcon];
