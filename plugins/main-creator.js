990871192b92b8d93969286737ea255eb92d08cf
DexterZ9 <dexterdp909@gmail.com>
2024-11-20 12:24:45 -0600

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,9 +1,8 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:Sisked ⭐;;\nFN:Sisked ⭐\nORG:Sisked ⭐\nTITLE:\nitem1.TEL;waid=584123989549:584123989549\nitem1.X-ABLabel:Sisked ⭐\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Sisked ⭐\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'おDanịel.xyz⁩', contacts: [{ vcard }] }}, {quoted: m})
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:Rudy:;;\nFN:Rudy:)\nTITLE:\nitem1.TEL;waid=50375961083:50375961083\nitem1.X-ABLabel:Rudy:)\nEND:VCARD`
+await conn.sendMessage(m.chat, { contacts: { displayName: 'Rudy:)', contacts: [{ vcard }] }}, {quoted: m})
 }
-handler.help = ['owner']
-handler.tags = ['main']
+
 handler.command = ['owner', 'creator', 'creador', 'dueño'] 
 
 export default handler
