c2803796d153670abd733dddd1e86c6eb15be4cf
DexterZ9 <dexterdp909@gmail.com>
2024-11-21 20:04:28 -0600

    Update owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -1,19 +1,11 @@
-import { spawn } from 'child_process';
-
-let handler = async (m, { conn, isROwner }) => {
-  if (!process.send) throw 'Usa: node index.js, no node main.js';
-  if (conn.user.jid === conn.user.jid) {
-    await m.reply('❇️ Reiniciando Bot...');
-    process.send('reset'); // Envía la señal de reinicio
-  } else {
-    throw 'No tienes permiso para usar este comando.';
-  }
+let handler = async (m) => {
+  await m.reply('⚙️ Reiniciando el bot...');
+  process.exit(0);  // Finaliza el proceso actual
 };
 
 handler.help = ['reiniciar'];
 handler.tags = ['owner'];
 handler.command = ['restart', 'reiniciar'];
-
 handler.rowner = true;
 
 export default handler;
